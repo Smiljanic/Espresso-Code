80a800b84209b9ae5f23c7acea1c3996395e3bed
Milena Smiljanic <itvms@nusselt.itv.uni-stuttgart.de>
2016-08-02 10:29:41 +0200

    com returns vector

--- src/core/cluster_analysis.cpp
+++ src/core/cluster_analysis.cpp
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2011,2012,2013,2014,2015,2016 The ESPResSo project
+  
+  This file is part of ESPResSo.
+  
+  ESPResSo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  ESPResSo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
 #include "cluster_analysis.hpp"
 #include "interaction_data.hpp"
 
@@ -41,31 +60,8 @@ void ClusterStructure::analyze_energy()
   }
 }
 
+
 /*
-//MILENA: further geometry analysis
-double * ClusterStructure::center_of_mass(Particle& p)  
-//iterate over particles within one cluster
-//take position of each particle
-//sum them up and divide with number of particles 
-{
-  //get size of cluster
-  double position[3] = {0,0,0};
-  int pid = 0;
-  while (cluster_id.find(pid) != cluster_id.end()) {
-    for (int i=0; i<3; i++)
-      position[i] = position[i] + p.r.p[i]; 
-  }
-  for (int j=0; j<3; j++) 
-    position[j]=position[j]*(1.0/sizeof(cluster_id));
-  return position;
-}
-//what is this code doing: sets position for com to 0,0,0
-//searchs for cluster_id[0]
-//ads to the position previous value of position and random 
-*/
-
-
-
 double * ClusterStructure::center_of_mass(Particle& p)  
 //iterate over particles within one cluster
 //take position of each particle
@@ -84,18 +80,18 @@ double * ClusterStructure::center_of_mass(Particle& p)
     position[j]=position[j]*(1.0/sizeof(cluster_id));
   return position;
 }
-
+*/
 
 
 // From Cluster get particles positions
-/*
-std::vector<double> Cluster::center_of_mass(Particle& p)  
+
+std::vector<double>  Cluster::center_of_mass(Particle& p) 
 {
   int cluster_size = particles.size();
   std::vector<double> position; //initialized com
   double temp[3] = {0,0,0}; //initialized position of particle
-  for (std::vector<int>::const_iterator it = particles.begin(); it != particles.end(); ++it) {
-//int pid = particles.find(it);
+  for (auto const& it : particles) {
+//    int pid = particles.find(it);
     int pid = particles[it]; //ID of the indexed particle from vector particles
 //MILENA: HERE TAKE PARTICLE POSITION AND STORE IT IN temp[3] 
     //ClusterStructure::cluster_id.find(p);
@@ -108,7 +104,7 @@ std::vector<double> Cluster::center_of_mass(Particle& p)
   }
   return position;
 }
-*/
+
 
 double Cluster::largest_distance(Particle& p)
 { 
@@ -119,10 +115,13 @@ double Cluster::largest_distance(Particle& p)
   double distance[3] = {0,0,0};
   double com[3];
 // here should be calculated com from the previous function
-//  com = Cluster::center_of_mass();
+// and position particle position taken for the
+// com = Cluster::center_of_mass();
   for (int p=0; p<cluster_size; p++){
+    int pid = particles[p];
     for (int i=0; i<3; i++) {
-     ld_vec[i] =position[i]-com[i]; 
+     //ld_vec[i] =position[i]-com[i]; 
+     ld_vec[i] = position[i]-com[i]; 
     }
     if (ld<sqrlen(ld_vec)) 
       ld=sqrlen(ld_vec);
